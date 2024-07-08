2719a364f840a698577872a979135283adebb09c
nicolamcgarry <nicola.mcgarry@btinternet.com>
2024-07-08 15:53:14 +0100

    url change for cors

--- src/Server.ts
+++ src/Server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'https://your-netlify-app-url.netlify.app', 
+  origin: 'https://nicolamcgarry.net', 
   optionsSuccessStatus: 200,
 };
 
